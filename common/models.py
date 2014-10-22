898323221b889ef8fa92c6e3409e7f87ae9fee09
vaputa <vaputa.leon@gmail.com>
2014-10-22 13:47:36 +0800

    Modify search form & add template

--- common/models.py
+++ common/models.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.db import models
 from django import forms
 import datetime
@@ -7,6 +8,6 @@ class LoginForm(forms.Form):
     password = forms.CharField(max_length = 20)
 
 class SearchForm(forms.Form):
-    capacity = forms.IntegerField(initial = 2)
-    order_begin = forms.DateField(initial=(datetime.date.today() + datetime.timedelta(1)))
-    order_end = forms.DateField(initial=(datetime.date.today() + datetime.timedelta(4)))
+    capacity = forms.IntegerField(initial = 2, widget=forms.NumberInput(attrs={'class': 'form-control'}), label='人数')
+    order_begin = forms.DateField(initial=(datetime.date.today() + datetime.timedelta(1)), widget=forms.TextInput(attrs={'class': 'form-control'}), label='入住日期')
+    order_end = forms.DateField(initial=(datetime.date.today() + datetime.timedelta(4)), widget=forms.TextInput(attrs={'class': 'form-control'}), label='离店日期')
